2c3d65f4bdc832acc2cc1fab6e4d9fa45a9f5d67
Susan Gakii <susangakii71@gmail.com>
2026-01-08 09:30:45 +0300

    adds flask restful create, post

--- server/app.py
+++ server/app.py
@@ -17,10 +17,60 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200
+        )
+
+        return response
+
+    def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            201
+        )
+
+        return response
+
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if not plant:
+            response = make_response(
+                {"error": "Plant not found"},
+                404
+            )
+            return response
+
+        response = make_response(
+            plant.to_dict(),
+            200
+        )
+
+        return response
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,9 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData())
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Numeric(10, 2))
